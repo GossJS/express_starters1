07551a687e008b6ebddc80413106e6f4d648500c
gossoudarev <igossoudarev@herzen.spb.ru>
2017-09-25 04:53:55 +0300

    improved this file simpletest.mjs

--- simpletest.mjs
+++ simpletest.mjs
@@ -2,12 +2,19 @@ import axios from 'axios';
 const URL = 'http://192.168.1.199:4321';
 const vals = [
   {val: "/add/5/6", expected:"11"},
-  {val: "/add/33/22", expected:"55"},
+  {val: "/add/133/22", expected:"155"},
   {val: "/add/0/13", expected:"13"}
 ];
 (async()=>{
    for (const o of vals) {
       const {data} = await axios.get(URL+o.val);
-      console.log(data);
+      console.log(`Got: ${String(data).padStart(6)} , ${o.expected.padEnd(6)} expected`);
    }
 })();
+
+
+/*
+/kramer/2/3/18/4/-5/-8 должно давать {"x":3,"y":4}
+/kramer/-2/4/-22/7/-5/50 должно давать {"x":5,"y":-3}
+
+*/
