474f0e34846ab331c95e1d6ed9c9b290ce17e0ff
EliasGoss <ibg2003@yandex.ru>
2017-09-25 19:35:15 +0300

    added live at classroom 25092017

--- index.mjs
+++ index.mjs
@@ -1,14 +1,28 @@
 import http from 'http';
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import myR from './routes/my';
+
+
+
+import compression from 'compression';
 const PORT=4321;
 const app=express();
 app
   .use(express.static('public'))
-  .use((r,res,n)=>console.log(r.url)||n())
-  .get('/hello',r=>r.res.end('Hello world!'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended:true}))
+
+  .use(compression())
+  .use((r,res,n)=>n())
+
+  .use('/my', myR(express))
+
+  .get(/hello/,r=>r.res.end('Hello world!'))
 
   .get('/add/:a/:b',r=> r.res.end(String(Number(r.params.a)+Number(r.params.b))))
- 
+
   .get('/err',()=>{throw 'Bad thing happened!'})
   .get('/err2',()=>{throw 'Awful thing happened!'})
 
