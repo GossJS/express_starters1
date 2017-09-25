08a3f0cb7b3639bea35c439a111925c1c31c5a7a
EliasGoss <ibg2003@yandex.ru>
2017-09-26 00:30:38 +0300

    path resolver

--- routes/users/index.mjs
+++ routes/users/index.mjs
@@ -2,10 +2,6 @@ import jsonfile  from 'jsonfile-promised';
 import path from 'path';
 const PATH = path.resolve('./users.json');
 
-
-
-
-
 import _ from 'lodash';
 
 export default x=>{
@@ -13,11 +9,10 @@ export default x=>{
 
   rtr
     .route('/')
-    .get(r=>{
-        jsonfile.readFile(PATH).then(x=>{
-          const result = '<ul>'+ x.users.map(x=>`<li>${x.login}</li>`).join(' ') + '</ul>';
-          r.res.send(result);
-        });
+    .get(async r=>{
+        const j = await jsonfile.readFile(PATH);
+        const result = '<ul>'+ j.users.map(x=>`<li>${x.login}</li>`).join(' ') + '</ul>';
+        r.res.send(result);
 
       })
     .post(r=>r.res.end('does not do anything') )
@@ -25,11 +20,13 @@ export default x=>{
   rtr
     .route('/add/:login/:password')
     .get(r=>r.res.end('You should use POST for that') )
-    .post(r=> {
+    .post(async r=> {
+       const j = await jsonfile.readFile(PATH);
        const {login, password} = r.params;
        if ( ! _.find(j.users, {login})   ) {
          j.users.push( {login, password}  );
          console.log( {login, password}  );
+         await jsonfile.writeFile(PATH,j);
 
          r.res.json({login, password});
        } else {
