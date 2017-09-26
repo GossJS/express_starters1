69bd3a4a0b577cb9a37c38f05eeb75ab105c4c7c
gossoudarev <igossoudarev@herzen.spb.ru>
2017-09-26 05:08:11 +0300

    refactored post and delete

--- routes/users/index.mjs
+++ routes/users/index.mjs
@@ -1,8 +1,7 @@
 import jsonfile  from 'jsonfile-promised';
 import path from 'path';
-const PATH = path.resolve('./users.json');
-
 import _ from 'lodash';
+const PATH = path.resolve('./users.json');
 
 export default x=>{
   const rtr = x.Router();
@@ -11,34 +10,33 @@ export default x=>{
     .route('/')
     .get(async r=>{
         const j = await jsonfile.readFile(PATH);
-        const result = '<ul>'+ j.users.map(x=>`<li>${x.login}</li>`).join(' ') + '</ul>';
-        r.res.send(result);
-
+        r.res.send(`<ol>${j.users.map(x=>`<li>${x.login}</li>`).join(' ')}</ol>`);
       })
-    .post(r=>r.res.end('does not do anything') )
+    .post(async r=> {
+       const j = await jsonfile.readFile(PATH);
+       //const {login, password} = r.params;
+       const {login, password} = r.body;
+       if (!login) return r.res.end('Nothing to add');
+       if ( ! _.find(j.users, {login})   ) {
+         j.users.push( {login, password}  );
+         await jsonfile.writeFile(PATH,j);
+         r.res.json({login, password});
+       } else {
+         r.res.send('User already exists');
+       }
+    })
+
   ;
 
 
 
   rtr
     .route('/:login/:password')
-    .get(r=>r.res.end('You should use POST for that') )
-    .post(async r=> {
-       const j = await jsonfile.readFile(PATH);
-       const {login, password} = r.params;
-       if ( ! _.find(j.users, {login})   ) {
-         j.users.push( {login, password}  );
-         console.log( {login, password}  );
-         await jsonfile.writeFile(PATH,j);
-
-         r.res.json({login, password});
-       } else {
-         r.res.send('User already exists');
-       }
-    })
+    .get(r=>r.res.end('You should use DELETE for that') )
     .delete(async r=> {
         const j = await jsonfile.readFile(PATH);
         const {login, password} = r.params;
+        if (!login) return r.res.end('Nothing to delete');
         const foundUser = _.find(j.users, {login});
         if ( foundUser) {
           if (foundUser.password == password) {
