e0d21d44217820bd2ff39af110ddf459ea73ee0c
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-03-09 00:51:23 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,21 +1,17 @@
-import http from 'http';
 import express from 'express';
+import fs from 'fs';
+
+const af=(fpath, line)=>new Promise((res,rej)=>fs.appendFile(fpath,line+'\n',(e,x)=>{if(e)rej(e);else res(String(x))}));
 const PORT=4321;
-const app=express();
-app
-  .use(express.static('public'))
-  .use((r,res,n)=>console.log(r.url)||n())
-  .get('/hello',r=>r.res.end('Hello world!'))
-
-  .get('/add/:a/:b',r=> r.res.end(String(Number(r.params.a)+Number(r.params.b))))
- 
-  .get('/err',()=>{throw 'Bad thing happened!'})
-  .get('/err2',()=>{throw 'Awful thing happened!'})
-
+express()
+  .get('/write/:what', async r => {
+    await af('lines.txt', `${r.url} ${r.params.what}`);
+    r.res.end('OK');
+  })
+  .get('/read', async r => {
+    fs.createReadStream('lines.txt').pipe(r.res);
+  })
   .use(r=>r.res.status(404).end('Still not here, sorry!'))
   .use((e,r,res,n)=>res.status(500).end(`Error: ${e}`))
-;
-http
-  .createServer(  app  )
   .listen(PORT, ()=>console.log(process.pid))
 ;
