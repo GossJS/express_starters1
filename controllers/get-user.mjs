ca38cf79059fbb06b732be1f81a1da4d29708bc6
Ilya Goss <goss@corp.ifmo.ru>
2019-11-11 20:05:46 +0300

    Update get-user.mjs

--- controllers/get-user.mjs
+++ controllers/get-user.mjs
@@ -1,8 +1,8 @@
 'use strict';
-import {User} from '../bd/mongoconn';
+import { User } from '../bd/mongoconn';
 
-export default async r=>{
-    const {username} = r.params;
-    const result = await User.findOne({username});
-    r.res.render('get-user', result  )
+export default async r => {
+    const { username } = r.params;
+    const result = await User.findOne({ username });
+    r.res.render('get-user', result);
 }
