ae68c509ac62e4359a1a7238dd35d4b73611af83
Ilya Goss <goss@corp.ifmo.ru>
2019-11-11 20:07:08 +0300

    Update get-users.mjs

--- controllers/get-users.mjs
+++ controllers/get-users.mjs
@@ -1,12 +1,12 @@
 'use strict';
-import {User} from '../bd/mongoconn';
+import { User } from '../bd/mongoconn';
 
-export default async r=>{
+export default async r => {
     const list = await User.find();
     r.res.render('get-users',
-      {o:list.map( x=> {
-           const {username, password} = x;
-           return {login: username, password};
-      }), title: 'Список пользователей'}
-    )
-}
+      { o: list.map(x => {
+           const { username, password } = x;
+           return { login: username, password };
+      } ), title: 'Список пользователей' }
+    );
+};
