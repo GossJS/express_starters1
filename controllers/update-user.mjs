c58abcbf149dc79c2dc1dcb5ab7019fc39b6d5cb
Ilya Goss <goss@corp.ifmo.ru>
2019-11-12 04:13:22 +0300

    Update update-user.mjs

--- controllers/update-user.mjs
+++ controllers/update-user.mjs
@@ -2,8 +2,8 @@
 import { User } from '../bd/mongoconn';
 
 export default async r => {
-  const {login, password, newpass} = r.body;
-  const x =   await User.update({ username: login, password }, { password: newpass });
+  const { login, password, newpass } = r.body;
+  const x = await User.update({ login, password }, { password: newpass });
   /* если пароль неверен, тоже будет nModified: 0  */
   if (x.nModified < 1) return r.res.end('no such user');
   r.res.json(x);
