521dff6fa7a38488b51c658a89ceeb5e508e7f39
Ilya Goss <goss@corp.ifmo.ru>
2019-11-12 04:08:02 +0300

    Update create-user.mjs

--- controllers/create-user.mjs
+++ controllers/create-user.mjs
@@ -3,8 +3,12 @@ import { User } from '../bd/mongoconn';
 
 export default async r => {
   const { login, password } = r.body;
-  const x  = await User.findOne({ username: login, password });
+  const x  = await User.findOne({ login, password });
   if (x) return r.res.send('User already exists!');
-  const newUser = new User( {username:login, password});
-  r.res.status(201).json( await newUser.save());
+  const newUser = new User( { login, password });
+  try {
+  	r.res.status(201).json(await newUser.save());
+  } catch (e) {
+  	r.res.status(500).end('Unable to perform operation!');
+  }
 };
