b3d59134294fd1eb4a5c4a85d5b5aa27d83643b6
Ilya Goss <goss@corp.ifmo.ru>
2019-11-12 03:46:59 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,39 +1,30 @@
 import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
+import compression from 'compression';
 
-import myR from './routes/my';
 import usersR from './routes/users';
 
-
-
-import compression from 'compression';
-const PORT=4321;
-const app=express();
+const PORT = 4321;
+const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+const app = express();
 
 app
   .use(express.static('public'))
   .use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended:true}))
-
+  .use(bodyParser.urlencoded({ extended: true }))
   .use(compression())
-  .use((r,res,n)=>n())
 
-  .use('/my', myR(express))
   .use('/users', usersR.rtr(express))
 
-  .get(/hello/,r=>r.res.end('Hello world!'))
+  .get(/hello/, r => r.res.end('Hello world!'))
 
-  .get('/add/:a/:b',r=> r.res.end(String(Number(r.params.a)+Number(r.params.b))))
-
-  .get('/err',()=>{throw 'Bad thing happened!'})
-  .get('/err2',()=>{throw 'Awful thing happened!'})
-
-  .use(r=>r.res.status(404).end('Still not here, sorry!'))
-  .use((e,r,res,n)=>res.status(500).end(`Error: ${e}`))
+  .use(({ res: r }) => r.status(404).set(hu).end('Пока ещё нет!'))
+  .use((e, r, rs, n) => rs.status(500).set(hu).end(`Ошибка: ${e}`))
   .set('view engine', 'pug')
 ;
-http
-  .createServer(  app  )
-  .listen(process.env.PORT || PORT, ()=>console.log(process.pid))
+
+export default http
+  .createServer(app)
+  .listen(process.env.PORT || PORT, () => console.log(process.pid))
 ;
